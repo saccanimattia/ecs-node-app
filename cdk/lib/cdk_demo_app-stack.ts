97bcf0109b0dca5ddb99d3f9c868c723bef8c5e3
saccanimattia <mattiasaccani1@gmail.com>
2025-10-06 15:24:10 +0200

    ecs

--- cdk/lib/cdk_demo_app-stack.ts
+++ cdk/lib/cdk_demo_app-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, ShellStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import * as env from 'dotenv';
+import * as path from 'path';
+
 
 env.config();
 
@@ -11,30 +16,68 @@ if (!GITHUB_OWNER || !GITHUB_REPO || !GITHUB_ARN) {
   throw new Error('Missing required environment variables: GITHUB_OWNER, GITHUB_REPO, GITHUB_ARN');
 }
 
-const PREFIX = 'ventie30-ecs-demo'
+const PREFIX = 'ventie30-ecs-demo';
 
 export class CdkDemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const shellStep = new ShellStep('Synth', {
-      input: CodePipelineSource.connection(`${GITHUB_OWNER}/${GITHUB_REPO}`, 'main',
-        { connectionArn: GITHUB_ARN! }
-      ),
-      commands: [
-        'cd cdk',
-        'npm ci',
-        'npx cdk synth'
-      ],
-      primaryOutputDirectory: 'cdk/cdk.out',
-      env: {
-        NODE_OPTIONS: '--max_old_space_size=4096',
-      },
+    // 🚀 PIPELINE
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: `${PREFIX}-pipeline`,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection(
+          `${GITHUB_OWNER}/${GITHUB_REPO}`,
+          'main',
+          { connectionArn: GITHUB_ARN! }
+        ),
+        commands: [
+          'cd cdk',
+          'npm ci',
+          'npx cdk synth',
+        ],
+        primaryOutputDirectory: 'cdk/cdk.out',
+      }),
     });
 
-    const codePipeline = new CodePipeline(this, 've-30-code-pipeline', {
-      pipelineName: `${PREFIX}-pipeline`,
-      synth: shellStep,
-    });
+    // 📦 FASE DI DEPLOY: ECS FARGATE
+    const deployStage = pipeline.addStage(new class extends cdk.Stage {
+      constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+        super(scope, id, props);
+
+        new class extends cdk.Stack {
+          constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+            super(scope, id, props);
+
+            const vpc = new ec2.Vpc(this, 'Vpc', {
+              ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+              maxAzs: 2,
+              vpcName: `${PREFIX}-vpc`,
+            });
+
+            const cluster = new ecs.Cluster(this, 'Cluster', {
+              vpc,
+              clusterName: `${PREFIX}-cluster`,
+            });
+
+            const service = new ApplicationLoadBalancedFargateService(this, 'Service', {
+              serviceName: `${PREFIX}-service`,
+              cluster,
+              cpu: 256,
+              memoryLimitMiB: 512,
+              desiredCount: 2,
+              taskImageOptions: {
+                containerPort: 80,
+                image: ecs.ContainerImage.fromAsset(path.join(__dirname, '../../src')),
+              },
+            });
+
+            service.targetGroup.configureHealthCheck({
+              path: '/',
+            });
+          }
+        }(this, 'EcsStack');
+      }
+    }(this, 'DeployStage'));
   }
 }
