9aae41ac8177b9a7fc02df730d2f4aa3d2d098da
saccanimattia <mattiasaccani1@gmail.com>
2025-10-06 14:59:51 +0200

    empty cdk

--- cdk/bin/cdk_demo_app.ts
+++ cdk/bin/cdk_demo_app.ts
@@ -5,3 +5,5 @@ import { CdkDemoAppStack } from '../lib/cdk_demo_app-stack';
 const app = new cdk.App();
 new CdkDemoAppStack(app, 'CdkDemoAppStack', {
 });
+
+app.synth();
--- cdk/lib/cdk_demo_app-stack.ts
+++ cdk/lib/cdk_demo_app-stack.ts
@@ -1,10 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, ShellStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import * as env from 'dotenv';
+
+env.config();
+
+const { GITHUB_OWNER, GITHUB_REPO, GITHUB_ARN } = process.env;
+
+if (!GITHUB_OWNER || !GITHUB_REPO || !GITHUB_ARN) {
+  throw new Error('Missing required environment variables: GITHUB_OWNER, GITHUB_REPO, GITHUB_ARN');
+}
 
 const PREFIX = 'ventie30-ecs-demo'
 
 export class CdkDemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const shellStep = new ShellStep('Synth', {
+      input: CodePipelineSource.connection(`${GITHUB_OWNER}/${GITHUB_REPO}`, 'main',
+        { connectionArn: GITHUB_ARN! }
+      ),
+      commands: [
+        'cd cdk',
+        'npm ci',
+        'npx cdk synth'
+      ],
+      primaryOutputDirectory: 'cdk/cdk.out',
+      env: {
+        NODE_OPTIONS: '--max_old_space_size=4096',
+      },
+    });
+
+    const codePipeline = new CodePipeline(this, 've-30-code-pipeline', {
+      pipelineName: `${PREFIX}-pipeline`,
+      synth: shellStep,
+    });
   }
 }
