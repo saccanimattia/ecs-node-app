f2be757ac38f4ca78f85f590eab04f4ce609d0d2
saccanimattia <mattiasaccani1@gmail.com>
2025-10-06 17:02:08 +0200

    hello lambda

--- cdk/lib/lambdaStack.ts
+++ cdk/lib/lambdaStack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface lambdaStackProps extends StackProps {
   stageName?: string;
@@ -8,6 +11,15 @@ interface lambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: lambdaStackProps) {
     super(scope, id, props);
+
+    new NodejsFunction(this, 'HelloHandler', {
+      runtime:  Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: join(__dirname, '..', 'src', 'hello.ts'),
+      environment: {
+        STAGE: props?.stageName!
+      }
+    });
   }
 
   
